5a8b5902e7e8a985b9add46403cac979941b3c51
filauro1989 <filauro1989@gmail.com>
2021-11-26 14:05:23 +0100

    first commit

---
+++ jsnack1/js/script.js
@@ -0,0 +1,11 @@
+let i = 0;
+let sum = 0
+
+while (i < 10) {
+    const numero = parseInt(prompt('inserisci un numero'));
+    sum += numero;
+    i++
+
+    console.log(numero);
+    console.log(sum);
+}
