62d3c96867e3f88c1474b144e85539e66610b7f3
filauro1989 <filauro1989@gmail.com>
2021-11-29 15:15:24 +0100

    fix ciclo for

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,11 +1,23 @@
-let i = 0;
+// let i = 0;
+// let sum = 0
+
+// while (i < 10) {
+//     const numero = parseInt(prompt('inserisci un numero'));
+//     sum += numero;
+//     i++
+
+//     console.log(numero);
+//     console.log(sum);
+// }
+
+// ciclo FOR 
+
 let sum = 0
 
-while (i < 10) {
+for (let index = 0; index < 10; index++) {
     const numero = parseInt(prompt('inserisci un numero'));
     sum += numero;
-    i++
-
-    console.log(numero);
-    console.log(sum);
-}-++
+    
+    console.log('numero utente', numero);
+    console.log('somma', sum);
+}
