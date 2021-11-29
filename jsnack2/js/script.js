64d3f24c07d28c7c377ebb963ff3d83e601435eb
filauro1989 <filauro1989@gmail.com>
2021-11-29 15:11:49 +0100

    fix with for

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -11,17 +11,30 @@ const nomeUtente = prompt('Digita il tuo nome').toLowerCase();
 // devo controllare nella lista invitati se c'è il nome 
 console.log(invitati);
 
-
 let find = false;
-let i = 0;
+// let i = 0;
 
-while (i < invitati.length && find == false) {
+// while (i < invitati.length && find == false) {
     
-    if (nomeUtente == invitati[i]) {
+//     if (nomeUtente == invitati[i]) {
+//         find = true;
+//     }
+//     i++
+// };
+
+// if (find == true) {
+//     alert('Perfetto, il tuo nome è nella lista')
+// } else {
+//     alert('Mi dispiace il tuo nome non è nella lista')
+// };
+
+for (let index = 0; index < invitati.length; index++) {
+
+    if (nomeUtente == invitati[index]) {
         find = true;
     }
-    i++
-};
+    
+}
 
 if (find == true) {
     alert('Perfetto, il tuo nome è nella lista')
